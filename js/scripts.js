fd9e2b8781ac992efa35e25cfa134b43509e56e4
silviabarasa16 <silviabarasa16@gmail.com>
2019-09-20 16:51:32 +0300

    Added some function and variable to my scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+function check(){
+    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
+    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    var birthday = document.getElementById("date").value;
+    var gender = document.getElementById("gender").value;
+    
+}
+
+
